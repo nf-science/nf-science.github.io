d7828637ba1516ac2a73e619edc7012542388fc4
横山 豪 <go.yokoyams+nfscience@gmail.com>
2019-04-15 17:20:21 +0900

    css

---
+++ js.js
@@ -0,0 +1,6 @@
+var rap_underbar = document.getElementById('rap_under_bar');
+
+
+function under_bar() {
+    rap_underbar.insertAdjacentHTML(afterbegin, <table><tr><td><a class="under_bar_a" id="under_home_a" href="./index.html"><p>ホーム&nbsp;&nbsp;</p></a></td><td><a class="under_bar_a" id="under_contet_a" href="./content.html"><p>活動内容&nbsp;&nbsp;</p></a></td><td><a class="under_bar_a" id="under_intro_a" href="./intro.html"><p>ロボット紹介&nbsp;&nbsp;</p></a></td><td><a class="under_bar_a" id="under_reconrds_a" href="recoed.html"><p>活動実績&nbsp;&nbsp;</p></a></td><td><a class="under_bar_a" id="under_enter_a" href="./enter.html"><p>入部案内&nbsp;&nbsp;</p></a></td><td><a class="under_bar_a" id="under_links_a" href="links.html"><p>リンク集&nbsp;&nbsp;</p></a></td></tr></table>);
+}
