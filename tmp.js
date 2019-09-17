3d6bea17b223c9c5aeb4c9902e87d565800057d3
Yokoyama-go-at-nfscience <go.yokoyams+nfscience@gmail.com>
2019-09-17 17:23:06 +0900

    create tmp for same code top bar

---
+++ tmp.js
@@ -0,0 +1,14 @@
+console.log('hello')
+
+function tmp_top() {
+    var html_top_write ='<div id="fixed"><div id="h1"><h1><span id="long_title">日本大学藤沢高等学校・中学校 科学部</span><span id="short_title">日藤科学部</span><h1></h1></div><div class="memus_div"><a class="memus" id="home_link" href="./index.html" data-wpel-link="internal">ホーム</a><a class="memus" id="content_link" href="./content.html" data-wpel-link="internal">活動内容</a><a class="memus" id="intro_link" href="./intro.html" data-wpel-link="internal">ロボット紹介</a><a class="memus" id="records_link" href="./recoed.html" data-wpel-link="internal">活動実績</a><a class="memus" id="enter_link" href="./enter.html" data-wpel-link="internal">入部案内</a></div></div>';
+
+   document.write(html_top_write);
+
+}
+
+function tmp_under() {
+    var html_under_write ='<div id="rap_under_bar"><div class="under_bar" id="under_bar"><div class="under_bar" id="index_underbar"><table><tr><td><a class="under_bar_a" id="under_home_a" href="./index.html"><p>ホーム&nbsp;&nbsp;</p></a></td><td><a class="under_bar_a" id="under_contet_a" href="./content.html"><p>活動内容&nbsp;&nbsp;</p></a></td <td><a class="under_bar_a" id="under_intro_a" href="./intro.html"><p>ロボット紹介&nbsp;&nbsp;</p></a></td><td><a class="under_bar_a" id="under_reconrds_a" href="recoed.html"><p>活動実績&nbsp;&nbsp;</p></a></td>         <td><a class="under_bar_a" id="under_enter_a" href="./enter.html"><p>入部案内&nbsp;&nbsp;</p></a></td><td><a class="under_bar_a" id="under_links_a" href="./links.html"><p>リンク集&nbsp;&nbsp;</p></a></td>      </tr></table></div>  </div></div><div class="under_bar_m"><table><tr><td><a href="./index.html" class="under_bar_m_a">ホーム</a></td></tr><tr><td><a href="./content.html" class="under_bar_m_a">活動内容</a></td></tr><tr><td><a href="./intro.html" class="under_bar_m_a">ロボット紹介</a></td></tr><tr><td><a href="./recoed.html" class="under_bar_m_a">活動実績</a></td></tr><tr><td><a href="./enter.html" class="under_bar_m_a">入部案内</a></td></tr><tr><td><a href="./links.html" class="under_bar_m_a">リンク集</a></td></tr></table></div>';
+
+    document.write(html_under_write)
+}
