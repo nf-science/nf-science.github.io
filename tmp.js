611fb3bf296f063bba0d39db87a0734a28347e22
yokoyama-go-at-nfscience <go.yokoyams+nfscience@gmail.com>
2019-12-24 16:01:44 +0900

    index以外もおわった...
    あとは比較して同じにするだけだ

--- tmp.js
+++ tmp.js
@@ -1,4 +1,3 @@
-
 function head() {
     var html_head_write = `
     <!-- Global site tag (gtag.js) - Google Analytics -->
@@ -20,9 +19,10 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 <!-- End Google Tag Manager -->
 
    <meta charset="utf-8">
-    <link rel="stylesheet" tpye="text/css" href="./style/both.css">
+       <link rel="stylesheet" tpye="text/css" href="./style/both.css">
        <link rel="stylesheet" type="text/css" href=" ./style/d.css" media="screen and (min-width:1000px) and (min-width:480px)"><!--For desktop users.-->
        <link rel="stylesheet" type="text/css" href="./style/m.css" media="screen and (max-width:999px)"><!--For mobile users.-->
+
         <meta name="theme-color" content="rgb(255, 177, 203)">
     
         <link href="./lib/Google-meterial.css" rel="stylesheet">
