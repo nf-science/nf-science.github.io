59af36c1640ac9d35833d419d5a7a7b346ea9024
yokoyama-go-at-nfscience <go.yokoyams+nfscience@gmail.com>
2020-02-08 15:10:37 +0900

    画像のサイズを変更

--- sw.js
+++ sw.js
@@ -2,11 +2,12 @@ const CACHE_NAME = 'nf-science\'s_cash';
 // Cache targets
 const urlsToCache = [
   './',
+  './img/favicon.png',
   './index.html',
-  '/style/both.css',
-  '/style/m.css',
-  '/style/d.css',
-  '/fonts/Noto_Sans_JP/NotoSansJP-Medium.otf',
+  './style/both.css',
+  './style/m.css',
+  './style/d.css',
+  './fonts/Noto_Sans_JP/NotoSansJP-Medium.otf',
 ];
 
 self.addEventListener('install', (event) => {
